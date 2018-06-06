0ebdf530dfb41d0b268912ac02a69f03266ad89c
Melissa Steffan <msteffan2012@gmail.com>
2018-06-06 15:27:32 -0400

    add sample project data

---
+++ src/projects-data.js
@@ -0,0 +1,45 @@
+// This is just some sample data so you don't have to think of your own!
+const projectData = {
+  project1: {
+    client: "Tronc",
+    name: "Los Angeles Times",
+    image: "/images/latimes.png",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dignissim massa in velit commodo semper. Nullam dignissim erat sed ex finibus, sed iaculis sem condimentum. Suspendisse ullamcorper tristique enim a vestibulum. Morbi rhoncus, augue vitae efficitur rutrum, ligula felis tincidunt neque, nec egestas felis elit quis odio. Orci varius natoque penatibus et magnis.",
+    status: "live"
+  },
+  project2: {
+    client: "Tronc",
+    name: "New York Daily News",
+    image: "/images/latimes.png",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dignissim massa in velit commodo semper. Nullam dignissim erat sed ex finibus, sed iaculis sem condimentum. Suspendisse ullamcorper tristique enim a vestibulum. Morbi rhoncus, augue vitae efficitur rutrum, ligula felis tincidunt neque, nec egestas felis elit quis odio. Orci varius natoque penatibus et magnis.",
+    status: "live"
+  },
+  project3: {
+    client: "Tronc",
+    name: "Chicago Tribune",
+    image: "/images/latimes.png",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dignissim massa in velit commodo semper. Nullam dignissim erat sed ex finibus, sed iaculis sem condimentum. Suspendisse ullamcorper tristique enim a vestibulum. Morbi rhoncus, augue vitae efficitur rutrum, ligula felis tincidunt neque, nec egestas felis elit quis odio. Orci varius natoque penatibus et magnis.",
+    status: "secret"
+  },
+  project4: {
+    client: "New Zealand Media and Entertainment",
+    name: "New Zealand Herald",
+    image: "/images/latimes.png",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dignissim massa in velit commodo semper. Nullam dignissim erat sed ex finibus, sed iaculis sem condimentum. Suspendisse ullamcorper tristique enim a vestibulum. Morbi rhoncus, augue vitae efficitur rutrum, ligula felis tincidunt neque, nec egestas felis elit quis odio. Orci varius natoque penatibus et magnis.",
+    status: "live"
+  },
+  project5: {
+    client: "Infobae",
+    name: "Infobae",
+    image: "/images/latimes.png",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dignissim massa in velit commodo semper. Nullam dignissim erat sed ex finibus, sed iaculis sem condimentum. Suspendisse ullamcorper tristique enim a vestibulum. Morbi rhoncus, augue vitae efficitur rutrum, ligula felis tincidunt neque, nec egestas felis elit quis odio. Orci varius natoque penatibus et magnis.",
+    status: "live"
+  }
+};
+
+export default projectData;
